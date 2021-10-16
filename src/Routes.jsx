0adae8a223d45fb27674950f26eeb59f879cb4e8
Saqlain1020 <saqlainprinters@gmail.com>
2021-10-16 21:17:38 +0500

    Table component done for top 100

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Top100 from "./pages/Top100/Top100";
 
 
 const Routes = () => {
   return (
     <Switch>
       <Route component={Home} path="/" exact />
-      
+      <Route component={Top100} path="/top100" exact />      
     </Switch>
   );
 };
